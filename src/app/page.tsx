c4819c53c24b864c6b0a4692b9f981f9636fc9cc
Baek Hyeon <bamstar.help@gmail.com>
2026-01-16 19:01:54 +0900

    Hide BlogSection on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
   return (
     <main className="min-h-screen bg-[#0A0A0A] text-white">
       <HeroSection />
-      <BlogSection posts={posts || []} />
+      {/* <BlogSection posts={posts || []} /> */}
       <FeatureSection />
       {/* <EventSection /> */}
       <CommunitySection />
